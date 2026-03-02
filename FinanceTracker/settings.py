7c8fde9f0263fd7c46cfe804e831a23f1c932abe
Himanshu79-ctrl <709himanshu@gmail.com>
2026-03-02 17:28:18 +0530

    Fixed production allowed hosts

--- FinanceTracker/settings.py
+++ FinanceTracker/settings.py
@@ -27,9 +27,9 @@ load_dotenv()
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost','finance-tracker-psxf.onrender.com',]
 
 
 # Application definition
