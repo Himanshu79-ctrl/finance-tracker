82a2e17c8ea54f6b81c5bcaaa6a932661dfefe90
Himanshu79-ctrl <709himanshu@gmail.com>
2026-03-03 20:44:21 +0530

    delete staticfiles_storage

--- FinanceTracker/settings.py
+++ FinanceTracker/settings.py
@@ -148,7 +148,6 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage' 
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
