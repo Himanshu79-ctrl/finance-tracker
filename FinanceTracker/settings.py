a945d9e2267c4200de331c6bdd35325c06a293bf
Himanshu79-ctrl <709himanshu@gmail.com>
2026-03-03 17:18:54 +0530

    Your message about the changes

--- FinanceTracker/settings.py
+++ FinanceTracker/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'static'
 STATICFILES_DIRS = [
-    'finance/static',
+    'FinanceTracker/static',
 ]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage' 
 
